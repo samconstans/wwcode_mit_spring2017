95c8bead2327df7d7743587af98eaaf20203f44f
Anastasiya Vovk <nastia-vovk@ukr.net>
2017-04-12 21:13:02 +0300

    optimized code in pSet1a for best practices

--- ps1a.py
+++ ps1a.py
@@ -5,37 +5,23 @@ Created on Tue Apr  4 13:37:52 2017
 @author: Анастасия
 """
 
-import math
-
-annual_salary = float(input('Enter your annual salary:'))
-portion_saved = input('Enter the percent of your salary to save, as a demimal:')
+annual_salary = int(input('Enter your annual salary:'))
+portion_saved = float(input('Enter the percent of your salary to save, as a demimal:'))
+total_cost = int(input('Enter the cost of your dream home:'))  
 current_saving = 0
-
-
-if float(portion_saved) > 1:
-    print('pease enter number from 0.1 to 1 ') 
-elif float(portion_saved) < 0.1:
-    print('It is should be at least 10%')    
-else:
-    current_saving = float(annual_salary) * float(portion_saved)
-        
+numbers_of_months = 0
+r = 0.04
 portion_down_payment = 0.25 #сколько реально нужно собрать первій взнос total_cost * portion_down_payment
 
-total_cost = float(input('Enter the cost of your dream home:'))    
-        
-r = 0.04  
-investments = float(current_saving)* r/12
+min_sum = portion_down_payment * total_cost
 
-base = 1 + r/12
+if portion_saved > 1:
+    print('pease enter number from 0.1 to 1 ') 
+elif portion_saved < 0.1:
+    print('It is should be at least 10%')  
+montly_salary = annual_salary/12
 
-print('base',base)
-
-A0 = int((total_cost * portion_down_payment)/(annual_salary * portion_saved))
-
-
-print('portion_down_payment/annual_salary*portion_saved',A0)
-
-
-numbers_of_months = math.log(A0, base)/5
-
-print(int(numbers_of_months))
+while current_saving <= min_sum:
+    current_saving += montly_salary * portion_saved + (current_saving * r/12)    
+    numbers_of_months += 1      
+print(numbers_of_months)

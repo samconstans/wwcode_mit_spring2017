8cecde1f254801b349a816741afef02e04c6b835
Anastasiya Vovk <nastia-vovk@ukr.net>
2017-04-12 10:08:11 +0300

    this is my first file on Python

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wwcode_mit_spring2017
+This is my first repository on Python
---
+++ first.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 10:04:57 2017
+
+@author: Анастасия
+"""
+
+print('Hello world')
